8385cc455666de3dec96cef1e336ec69b5877d49
parijatmitra <parijatmitra20@gmail.com>
2022-08-13 02:42:33 +0530

    gg

---
+++ index.js
@@ -0,0 +1,55 @@
+console.log('Hi');
+
+
+// let myBtn = document.querySelector('.myBtn');
+// let content = document.querySelector('.content');
+// let data1;
+// function getData(){
+//     url = 'https://api.github.com/users';
+//     fetch(url).then((response) => {
+//        return response.json();
+//     }).then((data) => {
+//         console.log(data);
+        
+//     }).catch(() => {
+       
+//     })
+// }
+
+// function postData(){
+//     url = 'https://jsonplaceholder.typicode.com/posts';
+//     data1 = {
+//         "userId": 1455667,
+//         "id": 62,
+//         "title": "beatae enim quia vel",
+//         "body": "enim aspernatur illo distinctio quae praesentium\nbeatae alias amet delectus qui voluptate distinctio\nodit sint accusantium autem omnis\nquo molestiae omnis ea eveniet optio"
+//       };
+//     params = {
+//         method: 'post', 
+//         headers: {
+//             'Content-Type': 'application/json'
+//         },
+//         body: JSON.stringify(data1)
+//     }
+//     fetch(url, params).then((response) => {
+//        return response.json();
+//     }).then((data) => {
+//         console.log(data);
+//     }).catch(() => {
+       
+//     })
+// }
+
+// postData();
+
+// // data1.forEach(element => {
+// //     console.log(element.login)
+// // });
+
+
+
+
+
+
+
+
