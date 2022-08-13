9040f4722723769ef15db7cc9c2a27a3430bb801
parijatmitra <parijatmitra20@gmail.com>
2022-08-14 04:52:33 +0530

    P13082022

--- index.js
+++ index.js
@@ -11,9 +11,9 @@ console.log('Hi');
 
 //     }).then((data) => {
 //         console.log(data);
-        
+
 //     }).catch(() => {
-       
+
 //     })
 //Go Champ Parijat Mitra
 // }
@@ -38,7 +38,7 @@ console.log('Hi');
 //     }).then((data) => {
 //         console.log(data);
 //     }).catch(() => {
-       
+
 //     })
 // }
 
@@ -50,6 +50,89 @@ console.log('Hi');
 
 
 
+
+
+// let a = 'Parijat Mita';
+// a = undefined;
+// if(a !== undefined){
+//     throw new Error('This is not undefined');
+// } else {
+//     console.log('Unidefined')
+// }
+// try {
+//    function1();
+// } catch (error) {
+//     console.log(error);
+//     console.error(error.message);
+// } finally {
+//     console.log('Go')
+// }
+let url = 'https://api.github.com/users';
+let printArea = document.querySelector('#content');
+console.log(printArea);
+let fetchBtn = document.querySelector('#fetch');
+let clearBtn = document.querySelector('#myBtn');
+let spinner = document.querySelector('#spin');
+console.log(spinner);
+fetchBtn.addEventListener('click', () => {
+    takeData();
+})
+
+let showTitleArray = [];
+clearBtn.addEventListener('click', () => {
+    spinner.style.display = "block";
+    printArea.innerHTML = ``;
+    showTitleArray = [];
+    localStorage.removeItem('title');
+    setTimeout(() => {
+        spinner.style.display = "none";
+    }, 500);
+   
+})
+
+showTitle();
+
+function takeData() {
+    spinner.style.display = "block";
+    fetch(url).then((response) => {
+        
+        return response.json();
+    }).then((data) => {
+        populateData(data);
+       
+    }).catch((error) => {
+        console.log(error);
+    })
+    setTimeout(() => {
+        spinner.style.display = "none";
+    }, 1000);
+}
+
+function showTitle() {
+    let data1 = localStorage.getItem('title');
+    if (!data1) {
+        showTitleArray = [];
+    } else {
+        showTitleArray = JSON.parse(data1);
+    }
+    if(showTitleArray.length > 0){
+    let html = `<ul class="list-group">`;
+    showTitleArray.forEach(element => {
+        html += `<li class="list-group-item">${element.login}</li>`
+    });
+    html += `</ul>`
+    printArea.innerHTML = html;
+}
+}
+
+function populateData(data) {
+    if (!data) {
+        return;
+    }
+
+    localStorage.setItem('title', JSON.stringify(data));
+    showTitle();
+}
 
 
 
